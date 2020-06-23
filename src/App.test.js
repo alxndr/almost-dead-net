64823c4d097dd4d4b11fa195a609c70559fd784e
Alexander <alxndr+github@gmail.com>
2020-06-23 00:17:41 -0700

    tap tap is this thing on

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders link to Twitter account', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText('@AlmostDeadNet on Twitter');
   expect(linkElement).toBeInTheDocument();
 });
