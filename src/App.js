d2d5e61fd1a6a1f1ae0efa415de651119de7f7a7
Alexander <alxndr+github@gmail.com>
2020-06-27 13:12:33 -0700

    add Sentry

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import * as Sentry from '@sentry/browser';
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
@@ -7,7 +8,10 @@ import routes from './routes'
 
 import './App.css'
 
+Sentry.init({dsn: "https://174349d7133f4877ba279f589bf54642@o412799.ingest.sentry.io/5293476"});
+
 function App() {
+  console.log('routes...', routes)
   return <>
     <div className={`App ${releaseStage()}`}>
       <a id="logo" href={routes.home}><img src="https://i.imgur.com/tvtgYVY.png" alt="Good Ol' Almost Dead" /></a>
@@ -22,7 +26,7 @@ function App() {
     <footer id="site-footer">
       Contributions / corrections:
       <br />
-      <a href="https://twitter.com/AlmostDeadNet" target="_new">@AlmostDeadNet on Twitter</a>
+      <a href="https://twitter.com/AlmostDeadNet" target="_new" rel="noopener noreferrer">@AlmostDeadNet on Twitter</a>
     </footer>
   </>
 }
