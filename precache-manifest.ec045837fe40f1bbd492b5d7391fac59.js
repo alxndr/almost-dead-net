a0040598c2810d1ad1a33dfd2b60e8f72ad9edf4
Alexander <alxndr+github@gmail.com>
2020-07-15 19:20:47 -0700

    Updates

---
+++ precache-manifest.ec045837fe40f1bbd492b5d7391fac59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543dcb04faf4eecce3c35d452940687c",
+    "url": "/almost-dead-net/index.html"
+  },
+  {
+    "revision": "efa51804b75a2d6f7d90",
+    "url": "/almost-dead-net/static/css/2.e22355b1.chunk.css"
+  },
+  {
+    "revision": "f4ec5fa5f16fce623c15",
+    "url": "/almost-dead-net/static/css/main.a7f5e2d8.chunk.css"
+  },
+  {
+    "revision": "efa51804b75a2d6f7d90",
+    "url": "/almost-dead-net/static/js/2.cbfd6756.chunk.js"
+  },
+  {
+    "revision": "016840ada0e9b532e9d813183c165378",
+    "url": "/almost-dead-net/static/js/2.cbfd6756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4ec5fa5f16fce623c15",
+    "url": "/almost-dead-net/static/js/main.fd58f914.chunk.js"
+  },
+  {
+    "revision": "1bd51437984f06b06063",
+    "url": "/almost-dead-net/static/js/runtime-main.ee5a381c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/almost-dead-net/precache-manifest.7226977db0ea5e1192a6baacef281d19.js"
+  "/almost-dead-net/precache-manifest.ec045837fe40f1bbd492b5d7391fac59.js"
 );
 
 self.addEventListener('message', (event) => {
