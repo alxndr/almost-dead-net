305c149ca01027b7a6d3a20a8e4c6b01f897e4e1
Alexander <alxndr+github@gmail.com>
2020-07-19 15:39:49 -0700

    make ShowsList nicer

--- src/components/shows_list.js
+++ src/components/shows_list.js
@@ -1,19 +1,24 @@
 import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
-import { filter, prop } from 'ramda'
+import { filter, find, prop, propEq } from 'ramda'
 
 import {parseWithCache} from '../fetch'
-import routes from '../routes';
+import routes from '../routes'
 
-import {SHOWS_URL} from '../data'
+import {
+  SHOWS_URL,
+  VENUES_URL,
+} from '../data'
 
 import './shows_list.css'
 
 const hasDate = filter(prop('date'))
 
-function ShowEntry({date, id}) {
-  return <li value={id}>
-    <Link to={routes.show.replace(':id', id)}>{date}</Link>
+function ShowEntry({children, show}) {
+  const {date, event, id, venue} = show
+  const title = `#${id}: ${date}${event ? ` ${event}` : ''} @ ${venue.name} (${venue.location})`
+  return <li>
+    <Link to={routes.show.replace(':id', id)} title={title}>{children || date}</Link>
   </li>
 }
 
@@ -21,16 +26,37 @@ const UNDEF = undefined
 
 export default function ShowsList() {
   const [shows, setShows] = useState(UNDEF)
+  const [venues, setVenues] = useState(UNDEF)
   useEffect(() => {
     parseWithCache(SHOWS_URL, (showData) => setShows(hasDate(showData)))
+    parseWithCache(VENUES_URL, (venueData) => setVenues(venueData))
+    // TODO change data stores to be object-based rather than array-based
   }, [])
-  if (shows === UNDEF) {
+  if (shows === UNDEF || venues === UNDEF) {
     return <p>Loading...</p>
   }
-  return <ol className="showslist">
-    {shows.length
-      ? shows.map(show => <ShowEntry key={show.id} date={show.date} id={show.id} />)
-      : <li>Uh oh, no shows found...</li>
+  const showsByYear = shows.reduce((acc, show) => {
+    const [m, d, y] = show.date.split('/') // e.g. === ['1', '26', '2013']
+    if (!acc[y]) acc[y] = {}
+    const venue = find(propEq('id', show.venue_id))(venues)
+    acc[y][`${m}/${d}`] = {
+      ...show,
+      venue
     }
-  </ol>
+    return acc
+  }, {})
+  return <ul className="showslist__years">
+    {Object.entries(showsByYear).reverse().map(([year, showsInYear]) => {
+      return <li className={`showslist__years--${year}`}>
+        <h3>{year}</h3>
+        <ul className="showslist__years__shows">
+          {Object.entries(showsInYear).map(([monthAndDay, show]) => {
+            return <li className={`showslist__years__shows__${monthAndDay}`}>
+              <ShowEntry key={show.id} show={show}>{monthAndDay}</ShowEntry>
+            </li>
+          })}
+        </ul>
+      </li>
+    })}
+  </ul>
 }
