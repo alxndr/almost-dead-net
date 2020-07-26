7c49afc66e69a2d51c324c3f197c239859dc4bbb
Alexander <alxndr+github@gmail.com>
2020-07-26 11:23:58 -0700

    reword Teases

--- src/components/teases_note.js
+++ src/components/teases_note.js
@@ -3,10 +3,11 @@ import React from 'react'
 import {Tooltip} from 'react-tippy'
 
 export default function TeasesNote({list}) {
-  const teases = `Tease(s): ${list.map(({song_name}) => song_name).join(', ')}`
-  return <Tooltip title={teases} trigger="mouseenter focus click">
+  const teases = list.map(({song_name}) => song_name).join(', ')
+  const text = `${teases} tease${list.length > 1 ? 's' : ''}`
+  return <Tooltip title={text} trigger="mouseenter focus click">
     <span className="setlist__songteases" aria-label="notes" role="img">🎧
-      <span className="hidden">{teases}</span>
+      <span className="hidden">{text}</span>
     </span>
   </Tooltip>
 }
