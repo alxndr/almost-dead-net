5c96a3110090eae4c5daa5b4d58dea7f8c096c77
Alexander <alxndr+github@gmail.com>
2020-08-05 01:28:56 -0700

    update all data from old live site!
    [#2]

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-const URL_BASE = 'https://gist.githubusercontent.com/alxndr/5f64cf477d5202c004856772ad2222db/raw/f4aab33aeed059c51e63b9ba1148b8e44c5bed62'
+const URL_BASE = 'https://gist.githubusercontent.com/alxndr/5f64cf477d5202c004856772ad2222db/raw/5f19d63f66e757011df505668f975c8cefe138fb'
 
 const GUESTS_URL = `${URL_BASE}/guests.csv`
 const SEGUES_URL = `${URL_BASE}/segues.csv`
