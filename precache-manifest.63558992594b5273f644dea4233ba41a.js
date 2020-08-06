0a8ed1ae27c967b25811e7fe07ee1d263e3b09ea
Alexander <alxndr+github@gmail.com>
2020-08-05 20:17:27 -0700

    Updates

---
+++ precache-manifest.63558992594b5273f644dea4233ba41a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9d234e89ac590f4df6044f009075c9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8887b649b8f5c0ee025c",
+    "url": "/static/css/2.e22355b1.chunk.css"
+  },
+  {
+    "revision": "c6fd253ffef72238f5b6",
+    "url": "/static/css/main.6629a2d4.chunk.css"
+  },
+  {
+    "revision": "8887b649b8f5c0ee025c",
+    "url": "/static/js/2.82cb33f8.chunk.js"
+  },
+  {
+    "revision": "4c4ecf11422d4ee203c598b84ae3b15e",
+    "url": "/static/js/2.82cb33f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6fd253ffef72238f5b6",
+    "url": "/static/js/main.71245e3d.chunk.js"
+  },
+  {
+    "revision": "5b782e139c76000bf64c",
+    "url": "/static/js/runtime-main.e88884bc.js"
+  }
+]);
