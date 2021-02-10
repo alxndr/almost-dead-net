54481c09a11b7d1d555b1d6e4148ced6aa75537a
Alexander <alxndr+github@gmail.com>
2021-02-10 00:02:42 -0800

    Home: link to Lot

--- src/pages/home.js
+++ src/pages/home.js
@@ -29,12 +29,13 @@ export default function Home(props) {
     <p>Pick a date to view the setlist and/or notes:</p>
     <ShowsByYear rawShows={shows} rawVenues={venues} />
     <hr />
+    <p><em>New!!</em> visit <a href="//lot.almost-dead.net" title="JRAD Forum: The Lot" style={{fontWeight:'bold'}}>The Lot</a> to discuss all things Joe Russo's Almost Dead!</p>
     <p>Coming eventually...</p>
     <ul>
       <li>easier navigation??</li>
       <li>more teases!</li>
-      <li>more links to Archive.org & Youtube</li>
       <li>fun stats</li>
+      <li><a href="https://lot.almost-dead.net/t/suggestions-for-the-main-site/18" title="leave a suggestion">your suggestions!</a></li>
     </ul>
   </section>
 }
