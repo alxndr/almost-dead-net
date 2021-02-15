0b9cc327d5f69839ff04cba664390dcc4d94dce6
Alexander <alxndr+github@gmail.com>
2021-02-15 10:46:53 -0800

    WIP config for `react-static`
    * `path` is required for each route
    * build is still erroring about `react-hot-loader/webpack`

--- static.config.js
+++ static.config.js
@@ -1,15 +1,23 @@
 const [, ...showIds] = Array(100).keys()
 
 export default {
-  getRoutes: async () => {
+  getRoutes: () => {
     return [
       {
+        path: '/',
+      },
+      {
+        path: '/show',
         children: showIds.map((showId) => {
           return {
-            path: `/show/${showId}`
+            path: showId.toString(), // relative to parent path
           }
-        })
-      }
+        }),
+      },
     ]
-  }
+  },
+  // getSiteData may be necessary?? https://github.com/react-static/react-static/blob/master/docs/config.md#getsitedata
+  maxThreads: 4, // be polite on this old Mac
+  productionSourceMaps: true,
+  siteRoot: 'https://almost-dead.net',
 }
