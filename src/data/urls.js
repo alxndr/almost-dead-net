145de6abb9cfdb6a805f0811f8d0f79bfc29c8a6
Alexander Quine <alxndr@users.noreply.github.com>
2021-03-22 11:29:59 -0700

    update data
    https://twitter.com/DustyDuds/status/1359026150169866243

--- src/data/urls.js
+++ src/data/urls.js
@@ -1,4 +1,4 @@
-const URL_BASE = 'https://gist.githubusercontent.com/alxndr/5f64cf477d5202c004856772ad2222db/raw/faf319e3d91a633199ec7087ae132e7d61bda902'
+const URL_BASE = 'https://gist.githubusercontent.com/alxndr/5f64cf477d5202c004856772ad2222db/raw/a37bb52b9bafbc1ac793168ca542d900a8a24978'
 
 const GUESTS_URL = `${URL_BASE}/guests.csv`
 const RECORDINGS_URL = `${URL_BASE}/recordings.csv`
