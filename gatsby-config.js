5c7450adbd6097b7425b9aee9dbb0dbb6dfa6044
Alexander <alxndr+github@gmail.com>
2021-03-25 18:19:18 -0700

    CSS tweaks; add Gatsby plugin for Google font

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,5 +32,15 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          'Shadows Into Light',
+          'Quicksand',
+        ],
+        display: 'swap',
+      },
+    },
   ],
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'gatsby'
 
 import Layout from '../components/layout'
 
@@ -6,15 +7,14 @@ import './about.css'
 
 export default function About() {
   return <Layout className="aboutPage">
-    <section>
-      <h1>About Almost-Dead.net</h1>
-      <p>In Memory of David Kay, creator of the <a href="https://docs.google.com/spreadsheets/d/1qxTKvLkiQgHycuC530OU3j-O5ggijHelXRXVjTiZLis/htmlview" target="_blank" rel="noopener noreferrer">JRAD-base spreadsheet</a></p>
-      <p>This site is not officially affiliated with JRAD.</p>
-      <hr />
-      <p>The content on this site was collected from public information.</p>
-      <p>The site on <tt>almost-dead.net</tt> is powered by <a href="https://github.com/alxndr/almost-dead-net" target="_blank" rel="noopener noreferrer">some JavaScript</a> using <a href="https://www.gatsbyjs.com/" target="_blank" rel="noopener noreferrer">Gatsby</a>, hosted on GitHub Pages.</p>
-      <p>The site on <tt>lot.almost-dead.net</tt> is <a href="https://lot.almost-dead.net/" title="JRAD Forum — The Lot">The Lot</a>, a forum powered by <a href="https://discourse.org" target="_blank" rel="noopener noreferrer">Discourse</a>, running on Google Cloud.</p>
-      <p>The domain name is managed by NameCheap.</p>
-    </section>
+    <h1>About Almost-Dead.net</h1>
+    <p>In Memory of <Link to="/show/184">David Kay</Link>, creator of the <a href="https://docs.google.com/spreadsheets/d/1qxTKvLkiQgHycuC530OU3j-O5ggijHelXRXVjTiZLis/htmlview" target="_blank" rel="noopener noreferrer">JRAD-base spreadsheet</a>…</p>
+    <p>This site is not officially affiliated with <a href="http://www.joerussosalmostdead.com/" target="_blank" rel="noopener">JRAD</a>.</p>
+
+    <p>The content on this site is based on public information, with additional contributions from a variety of ndividuals.</p>
+    <hr />
+    <p>The site on <tt>almost-dead.net</tt> is powered by <a href="https://github.com/alxndr/almost-dead-net" target="_blank" rel="noopener noreferrer">some JavaScript</a> using <a href="https://www.gatsbyjs.com/" target="_blank" rel="noopener noreferrer">Gatsby</a>, hosted on GitHub Pages.</p>
+    <p>The site on <tt>lot.almost-dead.net</tt> is powered by <a href="https://lot.almost-dead.net/" title="JRAD Forum — The Lot">The Lot</a>, a forum powered by <a href="https://discourse.org" target="_blank" rel="noopener noreferrer">Discourse</a>, running on Google Cloud.</p>
+    <p>The domain name is managed by NameCheap.</p>
   </Layout>
 }
