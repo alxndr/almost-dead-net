8aaded11b215b0ea9d4f4811225bea09e0c0e5c1
Alexander <alxndr+github@gmail.com>
2021-03-26 10:28:36 -0700

    static dir

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,11 +10,10 @@ export default function About() {
     <h1>About Almost-Dead.net</h1>
     <p>In Memory of <Link to="/show/184">David Kay</Link>, creator of the <a href="https://docs.google.com/spreadsheets/d/1qxTKvLkiQgHycuC530OU3j-O5ggijHelXRXVjTiZLis/htmlview" target="_blank" rel="noopener noreferrer">JRAD-base spreadsheet</a>…</p>
     <p>This site is not officially affiliated with <a href="http://www.joerussosalmostdead.com/" target="_blank" rel="noopener">JRAD</a>.</p>
-
-    <p>The content on this site is based on public information, with additional contributions from a variety of ndividuals.</p>
+    <p>The content on this site is based on public information, with additional contributions from a variety of individuals.</p>
     <hr />
     <p>The site on <tt>almost-dead.net</tt> is powered by <a href="https://github.com/alxndr/almost-dead-net" target="_blank" rel="noopener noreferrer">some JavaScript</a> using <a href="https://www.gatsbyjs.com/" target="_blank" rel="noopener noreferrer">Gatsby</a>, hosted on GitHub Pages.</p>
-    <p>The site on <tt>lot.almost-dead.net</tt> is powered by <a href="https://lot.almost-dead.net/" title="JRAD Forum — The Lot">The Lot</a>, a forum powered by <a href="https://discourse.org" target="_blank" rel="noopener noreferrer">Discourse</a>, running on Google Cloud.</p>
+    <p>The site on <tt>lot.almost-dead.net</tt> is <a href="https://lot.almost-dead.net/" title="JRAD Forum — The Lot">The Lot</a>, a forum powered by <a href="https://discourse.org" target="_blank" rel="noopener noreferrer">Discourse</a>, running on Google Cloud.</p>
     <p>The domain name is managed by NameCheap.</p>
   </Layout>
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -22,8 +22,8 @@ const IndexPage = ({pageContext: {shows, venues, songs}}) => (
       <h2>Links:</h2>
       <ul>
         <li><a href="https://lot.almost-dead.net" title="JRAD Forum: The Lot">forum: <img src="https://lot.almost-dead.net/uploads/default/original/1X/71ea69cde5d1aedbf81ce09bdd8acf299d582546.png" alt="The Lot (message board / forum)" style={{maxHeight:'2em',verticalAlign:'middle'}} /></a></li>
-        <li><a href="/about" title="About Almost-Dead.net">about the site</a></li>
         <li><a href="https://lot.almost-dead.net/c/meta/2" title="Meta category at The Lot">corrections & suggestions</a></li>
+        <li><a href="/about" title="About Almost-Dead.net">about the site</a></li>
       </ul>
     </section>
     <section className="homepage__showsbyyear">
