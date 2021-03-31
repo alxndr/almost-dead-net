6f3825cd6fd0dc496ebef8ea358f7843fe186a29
Alexander <alxndr+github@gmail.com>
2021-03-30 18:24:35 -0700

    remove unused data

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
-const Header = ({ siteTitle }) => (
+const Header = ({}) => (
   <header style={{}}>
     <Link className="App__banner" to="/" title="Almost-Dead.net home page">
       <img src="https://i.imgur.com/q5cBEHG.png" alt="Almost-Dead.net banner" />
@@ -11,11 +11,9 @@ const Header = ({ siteTitle }) => (
 )
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
 }
 
 export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -10,25 +10,14 @@ import './reset.css'
 import './layout.css'
 
 const Layout = ({children, className = ''}) => {
-  const data = {site: {siteMetadata: {title: 'a title.....'}}}
-  //const data = useStaticQuery(graphql`
-  //  query SiteTitleQuery {
-  //    site {
-  //      siteMetadata {
-  //        title
-  //      }
-  //    }
-  //  }
-  //`)
-
   return (
     <div className={`layout layout-${releaseStage()} ${className}`}>
-      <Header siteTitle={data.site.siteMetadata?.title || ''} />
+      <Header />
       <main>{children}</main>
       <footer id="site-footer" className="footer">
         <Link className="footer__logo" id="logo-bottom" to="/" title="return to home page"><img src="https://i.imgur.com/tvtgYVY.png" alt="Good Ol' Almost Dead" /></Link>
         <Link className="footer__link-about" to="/about" title="About the Site">About the Site</Link>
-        <Link className="footer__logo-lot" to="https://lot.almost-dead.net" title="JRAD Forum — The Lot"><img src="https://i.imgur.com/Qi2NhJO.png" alt="The Lot — a forum for fans of Joe Russo's Almost Dead" /></Link>
+        <a href="https://lot.almost-dead.net" className="footer__logo-lot" title="JRAD Forum — The Lot"><img src="https://i.imgur.com/Qi2NhJO.png" alt="The Lot — a forum for fans of Joe Russo's Almost Dead" /></a>
       </footer>
     </div>
   )
