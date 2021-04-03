f202414ea424cedad63cdf237471eaa72f339321
Alexander <alxndr+github@gmail.com>
2021-04-02 19:56:33 -0700

    Deploy deploy-2021-04-02-195233-PDT

---
+++ component---src-pages-venue-js-f12b71b1701a713b7f18.js
@@ -0,0 +1,2 @@
+(self.webpackChunkalmost_dead_net=self.webpackChunkalmost_dead_net||[]).push([[408],{8495:function(e,n,t){"use strict";t.r(n),t.d(n,{default:function(){return u}});var a=t(7294),l=t(4250),s=t(5581);function u(e){var n=e.pageContext,t=n.venue,u=n.shows;return console.log({venue:t,shows:u}),a.createElement(s.Z,{className:"venuepage"},a.createElement(l.Z,{title:"JRAD shows at "+t.name+", "+t.location}),a.createElement("h1",null,"Whjeeeeeeee"),a.createElement("p",null,t.name))}}}]);
+//# sourceMappingURL=component---src-pages-venue-js-f12b71b1701a713b7f18.js.map
