8929aee117884c0b2ce1ff8574d6b23b2309c806
Alexander <alxndr+github@gmail.com>
2021-04-18 17:51:16 -0700

    Songs page separates performed vs teased
    * and will show the author of a song if it's a non-cover Dead song
    * and update data

--- src/pages/songs.js
+++ src/pages/songs.js
@@ -16,35 +16,49 @@ function Author({name}) {
     {name}
   </span>
 }
-function SongLink({data: {author, core_gd, id, performances, suite, title, ...rest}}) {
+function SongLink({data: {author, core_gd, cover_gd, id, suite, title, ...rest}}) {
   return <li>
     <Link to={`/song/${id}`}>
       "{title}"
       {suite && <span className="song__suite">{suite} suite</span>}
-      {!core_gd && <Author name={author} />}
+      {(!core_gd || cover_gd) && <Author name={author} />}
     </Link>
   </li>
 }
 
 export default function SongsPage({pageContext: {songs}}) {
   const songsClean = songs.filter(songData => songData.title && songData.title !== '[unknown]')
-  const groupedBySuite = groupBy(
-    (songData) => songData.suite,
+  const groupedByPerformedVsTeased = groupBy(
+    (songData) => Boolean(songData.performances),
     songsClean
   )
-  const songsAndSuites = [
-    ...groupedBySuite[''],
-    {title: 'Terrapin Suite',       sections: groupedBySuite['Terrapin']},
-    {title: 'Weather Report Suite', sections: groupedBySuite['Weather Report']},
-  ]
+  const performedSongs = groupedByPerformedVsTeased[true]
+  const teasedSongs = groupedByPerformedVsTeased[false]
+
+  //const groupedBySuite = groupBy(
+  //  (songData) => songData.suite,
+  //  performedSongs
+  //)
+  //const songsAndSuites = [
+  //  ...groupedBySuite[''],
+  //  {title: 'Terrapin Suite',       sections: groupedBySuite['Terrapin']},
+  //  {title: 'Weather Report Suite', sections: groupedBySuite['Weather Report']},
+  //]
+
   return <Layout className="songs">
     <SEO
       title="JRAD — all songs played"
       description="Almost-complete repertoire of Joe Russo's Almost Dead (JRAD) songs and teases"
     />
-    <h1>Songs (teased & performed)</h1>
+    <p><a href="#songs__headline--teased">Jump down to "Teases Only"</a></p>
+    <h1>Songs Performed / Jammed</h1>
     <ul className="songs__list">
-      {sortBy((prop('title')))(songsClean)
+      {sortBy((prop('title')))(performedSongs)
+        .map(songData => songData.sections ? songData.title : <SongLink data={songData} />)}
+    </ul>
+    <h1 id="songs__headline--teased">Songs Teased</h1>
+    <ul className="songs__list">
+      {sortBy((prop('title')))(teasedSongs)
         .map(songData => songData.sections ? songData.title : <SongLink data={songData} />)}
     </ul>
   </Layout>
