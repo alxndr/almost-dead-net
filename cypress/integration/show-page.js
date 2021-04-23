df653dedfdc47926298482361b83fea56315feda
Alexander <alxndr+github@gmail.com>
2021-04-22 17:34:43 -0700

    test: cover more parts of Show pages

--- cypress/integration/show-page.js
+++ cypress/integration/show-page.js
@@ -1,9 +1,29 @@
 describe('Show page', () => {
-  beforeEach(() => {
-    cy.visit('/show/38')
+  it('lists Show notes', () => {
+    cy.visit('/show/85')
+    cy.get('main')
+      .contains('acoustic')
+      .contains('longest show to date')
   })
 
-  it('Song info', () => {
+  it('lists Recordings', () => {
+    cy.visit('/show/35')
+    cy.get('main')
+      .find('a')
+      .contains('audience')
+      .should('have.attr', 'href', 'https://archive.org/details/JRAD2015-06-05.nyctaper')
+  })
+
+  it('lists Guests', () => {
+    cy.visit('/show/176')
+    cy.get('main')
+      .contains('Red Baraat')
+      .contains('Jonathan Goldberger')
+  })
+
+  it('lists Song info', () => {
+    cy.visit('/show/38')
+
     cy.get('main')
       .find('a')
       .as('links')
@@ -13,23 +33,29 @@ describe('Show page', () => {
     cy.get('@links')
       .contains('Slipknot!')
 
-    cy.log('FTP marker')
+    cy.log('"first time played" marker (on Mama Tried)')
     cy.get('main')
       .find('.setlist__set-1 > ol > li:first-child')
       .contains('first')
 
-    cy.log('Tease')
+    cy.log('Tease mark (on UJB)')
     cy.get('main')
       .find('.setlist__songteases')
       .first()
-      .click()
+      .click() // will trigger the hover behavior
     cy.contains('Estimated Prophet tease')
 
     cy.log('tracklist notes')
     cy.get('main')
       .find('.setlist__songnote')
       .first()
-      .click()
+      .click() // will trigger the hover behavior
     cy.contains('broken pedal')
+  })
+
+  it('shows a lead image', () => {
+    cy.visit('/show/52')
+    cy.get('main')
+      .find('img', /imgur/)
   })
 })
