77b83a3ae75ba4bca3e2b8d7f7bc7db2cf55bff5
Alexander <alxndr+github@gmail.com>
2021-05-14 09:56:37 -0700

    Updates

---
+++ precache-manifest.a89a370a8fdb815a8fd19d8dfda06cfc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c1162685f77ce3c497ce4b31eb715cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc9728133b9fee044856",
+    "url": "/static/css/2.e22355b1.chunk.css"
+  },
+  {
+    "revision": "2ae47c06eea3f15874b0",
+    "url": "/static/css/main.22ca132a.chunk.css"
+  },
+  {
+    "revision": "cc9728133b9fee044856",
+    "url": "/static/js/2.b8ed16a6.chunk.js"
+  },
+  {
+    "revision": "c150e6c33658ae5f7516148f2c02e1e5",
+    "url": "/static/js/2.b8ed16a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ae47c06eea3f15874b0",
+    "url": "/static/js/main.d8e16901.chunk.js"
+  },
+  {
+    "revision": "5b782e139c76000bf64c",
+    "url": "/static/js/runtime-main.e88884bc.js"
+  }
+]);
