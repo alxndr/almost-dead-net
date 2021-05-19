75c22321e77facf7913ed47718a159bbf9d4ad23
Alexander <alxndr+github@gmail.com>
2021-05-19 13:49:24 -0700

    debug: WIP Home page adding widget for Discourse forum

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
-import * as React from "react"
+import React, {useEffect} from "react"
+import { Helmet } from "react-helmet"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
@@ -10,28 +11,64 @@ import ShowsByYear from '../components/shows_by_year'
 
 import './home.css'
 
-const IndexPage = ({pageContext: {shows, venues, songs}}) => (
-  <Layout className="homepage">
+function isDebugMode() {
+  return global.window.location.hash === '#debug'
+}
+
+const IndexPage = ({pageContext: {shows, venues, songs}}) => {
+  useEffect(() => {
+    if (!isDebugMode()) return null
+    const [containerElement] = document.getElementsByClassName('homepage__forum')
+    const existingElements = containerElement.getElementsByTagName('d-topics-list')
+    if (existingElements)
+      for (const elem of existingElements)
+        elem.remove()
+    console.debug('container', containerElement)
+    const newElement = document.createElement('d-topics-list')
+    // <d-topics-list discourse-url="https://lot.almost-dead.net" per-page="5" status="open"></d-topics-list>
+    newElement.setAttribute('discourse-url', 'https://lot.almost-dead.net')
+    newElement.setAttribute('per-page', '5')
+    newElement.setAttribute('status', 'open')
+    console.debug('new...', newElement)
+    containerElement.appendChild(newElement)
+  }, [])
+
+  return <Layout className="homepage">
+    <Helmet>
+      <script src="https://lot.almost-dead.net/javascripts/embed-topics.js" type="text/javascript"></script>
+    </Helmet>
+
     <SEO title="Almost-Dead.net" description="Joe Russo's Almost Dead setlists, teases, performance recordings, discussion forum" />
-    <h1>Setlists, segues, teases by JRAD</h1>
+
+    <h1>JRAD forum &amp; setlists with teases &amp; segues</h1>
+
+    {global.window.location.hash === '#debug'
+      ? <section className="homepage__forum">
+        <h2><a href="https://lot.almost-dead.net" title="JRAD Forum: The Lot"><img src="https://lot.almost-dead.net/uploads/default/original/1X/71ea69cde5d1aedbf81ce09bdd8acf299d582546.png" alt="The Lot (message board / forum)" style={{maxHeight:'1.7em',verticalAlign:'middle'}} /></a></h2>
+      </section>
+      : null
+    }
+
     <section className="homepage__songfinder">
       <h2>Find a song:</h2>
       <SongFinder songs={songs} />
     </section>
+
     <section className="homepage__links">
       <h2>Links:</h2>
       <ul>
-        <li><em>New page!</em> <Link to="/songs" title="All songs performed or teased by JRAD">All Songs Played or Teased</Link></li>
+        <li><Link to="/songs" title="All songs performed or teased by JRAD">All Songs Played or Teased</Link></li>
         <li><a href="https://lot.almost-dead.net/c/meta/2" title="Meta category at The Lot">corrections & suggestions</a></li>
         <li><a href="/about" title="About Almost-Dead.net">about the site</a></li>
-        <li><a href="https://lot.almost-dead.net" title="JRAD Forum: The Lot">forum: <img src="https://lot.almost-dead.net/uploads/default/original/1X/71ea69cde5d1aedbf81ce09bdd8acf299d582546.png" alt="The Lot (message board / forum)" style={{maxHeight:'1.7em',verticalAlign:'middle'}} /></a></li>
       </ul>
     </section>
+
     <section className="homepage__showsbyyear">
       <h2>Find a show:</h2>
       <ShowsByYear rawShows={shows} rawVenues={venues} />
     </section>
+
   </Layout>
-)
+}
 
 export default IndexPage
