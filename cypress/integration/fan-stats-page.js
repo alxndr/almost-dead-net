be82564f318ac20a1fda72f7ff46b1790d113fee
Alexander <alxndr+github@gmail.com>
2021-09-21 14:11:39 -0700

    test: initial verification of Fan Stats page
    [#321]

---
+++ cypress/integration/fan-stats-page.js
@@ -0,0 +1,38 @@
+describe('Fan Stats page', () => {
+  beforeEach(() => {
+    cy.visit('/fan-stats')
+    cy.get('main')
+      .as('main')
+  })
+
+  it('renders', () => {
+    cy.get('@main')
+      .contains('Fan Stats')
+    cy.get('@main')
+      .find('input[type=text]')
+      .should('have.attr', 'placeholder', 'your username on The Lot')
+    cy.get('@main')
+      .find('button')
+      .contains('generate stats')
+  })
+
+  describe('entering a username', () => {
+    it('shows some totals', () => {
+      cy.get('@main')
+        .find('input[type=text][placeholder*=username]')
+        .type('theengineer')
+      cy.get('@main')
+        .find('button')
+        .contains('generate stats')
+        .click()
+      cy.get('@main')
+        .contains(`@TheEngineer's Fan Stats`)
+      cy.get('@main')
+        .contains('21 shows')
+        .contains('10 venues')
+        .contains('130 different songs')
+        .contains('77 unique songs')
+    })
+  })
+
+})
