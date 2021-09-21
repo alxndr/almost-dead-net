2fdac2ff78e6a54d4053f588d6797b67ab17952c
Alexander <alxndr+github@gmail.com>
2021-09-20 17:00:01 -0700

    feature: list of Shows and Venues on the Fan Stats page
    [#321]

--- src/pages/fan-stats.js
+++ src/pages/fan-stats.js
@@ -1,7 +1,12 @@
 import React, {useEffect, useState} from 'react'
+import {graphql, Link} from 'gatsby'
+import {find, propEq, uniq, __} from 'ramda'
+import slugify from 'slugify'
 
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+
+import {pluralize} from '../helpers/string_helpers'
 
 import './fan-stats.css'
 
@@ -12,43 +17,137 @@ const UsernameForm = () => <form action="/fan-stats">
 
 const LinkedUser = ({username}) => <a href={`https://lot.almost-dead.net/u/${username}/summary`}>@{username}</a>
 
-const Title = ({username}) => {
+const PageTitle = ({username}) => {
   if (username) {
     return <>Fan Stats for <LinkedUser username={username} /></>
   }
   return 'Fan Stats'
 }
 
-export default function FanStatsPage({location}) {
-  const params = new URLSearchParams(location.search)
-  const username = params.get('username')
+const venueURL = ({id, name}) => `/venue/${id}-${slugify(name)}`
 
-  const [data, setData] = useState({})
+const FanStats = ({user, shows}) => {
+  const showIDs = Object.keys(shows)
+  const venues = uniq(Object.values(shows).map(({venue}) => venue))
+
+  return <>
+    <p><LinkedUser username={user.username} /> has seen {showIDs.length} shows at {pluralize(venues.length, 'venue')}.</p>
+
+    <h2>Shows</h2>
+    <ol>
+      {showIDs.map((showID) => {
+        const showData = shows[showID]
+        return <li>
+          <Link to={`/show/${showID}`}>{showData.date}</Link>
+          {' at '}
+          <Link to={venueURL(showData.venue)}>{showData.venue.name}, {showData.venue.location}</Link>
+        </li>
+      })}
+    </ol>
+
+    <h2>Venues</h2>
+    <ol>
+      {venues.map((venue) => <li><Link to={venueURL(venue)}>{venue.name}, {venue.location}</Link></li>)}
+    </ol>
+  </>
+}
+
+export default function FanStatsPage({location, data}) {
+  const usernameRaw = new URLSearchParams(location.search).get('username')
+
+  const [userJson, setUserJson] = useState()
+  const [showsData, setShowsData] = useState()
+
   useEffect(() => {
-    if (username) {
-      fetch(`https://lot.almost-dead.net/u/${username}.json`)
+    if (usernameRaw) {
+      fetch(`https://lot.almost-dead.net/u/${usernameRaw}.json`)
         .then((response) => response.json())
-        .then((data) => {
-          if (data.user && data.user.user_fields && data.user.user_fields[1].length)
-            setData({shows: JSON.parse(data.user.user_fields[1])})
-          else
-            setData({error: true})
+        .then((json) => {
+          global.console.log(json)
+          if (json?.user) {
+            setUserJson({
+              user: json.user,
+              shows: json.user.user_fields[1]?.length
+                ? JSON.parse(json.user.user_fields[1])
+                : [],
+            })
+          }
         })
     }
-  }, [username])
+  }, [usernameRaw])
+
+  useEffect(() => {
+    if (userJson?.shows?.length) {
+      const findShow = find(__, shows)
+      const findVenue = find(__, venues)
+      setShowsData(userJson.shows.reduce((showsData, showDateString) => {
+        const showDMYYYY = showDateString.split(' ')[0].replace(/\/(\d{2})$/, '/20$1')
+        const showData = findShow(propEq('date', showDMYYYY))
+        if (showData?.id) {
+          const venue = findVenue(propEq('id', showData.venue_id))
+          return {
+            ...showsData,
+            [showData.id]: {
+              ...showData,
+              venue,
+            },
+          }
+        }
+        return showsData
+      }, {}))
+    }
+  }, [userJson])
+
+  const {
+    allShowsCsv: {nodes: shows},
+    allVenuesCsv: {nodes: venues},
+  } = data
 
   return <Layout className="fanstatspage">
     <SEO title="JRAD Fan Stats" />
-    <h1><Title username={username} /></h1>
-    {data.error
-      ? <><p>Uh oh, error fetching data.</p><UsernameForm/></>
-      : data.shows
-        ? data.shows.length
-          ? <p><LinkedUser username={username} /> has seen {data.shows.length} shows.</p>
+    <h1><PageTitle username={userJson?.user?.username} /></h1>
+    {userJson?.user
+      ? showsData
+        ? Object.keys(showsData).length
+          ? <FanStats shows={showsData} user={userJson.user} />
           : <><p>No shows found!</p><UsernameForm/></>
-        : username
+        : usernameRaw
           ? <p>Loading...</p>
           : <UsernameForm/>
+      : <><p>Uh oh, error fetching userJson.</p><UsernameForm/></>
     }
   </Layout>
 }
+
+export const query = graphql`
+  query FanStatsData {
+    allVenuesCsv {
+      nodes {
+        id
+        name
+        location
+        capacity
+        generic_name
+        tagname
+      }
+    }
+    allShowsCsv {
+      nodes {
+        date
+        encore1
+        encore2
+        event
+        id
+        links
+        notes
+        num_recordings
+        set1
+        set2
+        set3
+        soundcheck
+        tagline
+        venue_id
+      }
+    }
+  }
+`
