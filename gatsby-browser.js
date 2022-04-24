aedc88748b3fed1f02015c982b0e203fb89e524c
Alexander <alxndr+github@gmail.com>
2022-04-23 22:17:25 -0700

    ops: remove "loadable" things
    https://github.com/gatsbyjs/gatsby/discussions/32860#discussioncomment-2619170

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,22 +4,7 @@
  * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
 
-const {hydrate} = require('react-dom')
-const {loadableReady} = require('@loadable/component')
 
 exports.disableCorePrefetching = () => true
 
-exports.replaceHydrateFunction = () => (element, container, callback) => {
-  loadableReady(() => {
-    hydrate(element, container, callback)
-  })
-}
 
-exports.onRouteUpdate = () => {
-  // h/t @samblackk https://github.com/gatsbyjs/gatsby/issues/10410#issuecomment-739031719
-  window.locations = window.locations || [document.referrer];
-  if (window.locations[locations.length - 1] !== window.location.href) {
-    window.locations.push(window.location.href);
-  }
-  window.previousPath = window.locations[locations.length - 2];
-}
--- gatsby-node.js
+++ gatsby-node.js
@@ -162,22 +162,3 @@ exports.createPages = async ({graphql, actions: {createPage, createTypes} }) => 
   })
 }
 
-exports.onCreateWebpackConfig = ({ actions, stage }) => {
-  if (
-    stage === "build-javascript" ||
-    stage === "develop" ||
-    stage === "develop-html"
-  ) {
-    actions.setWebpackConfig({
-      plugins: [
-        new LoadablePlugin({
-          filename:
-            stage === "develop"
-              ? `public/loadable-stats.json`
-              : "loadable-stats.json",
-          writeToDisk: true
-        })
-      ]
-    });
-  }
-};
