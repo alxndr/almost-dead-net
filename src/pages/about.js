51af76140d53817c3c5e6e107d319a55be300167
Alexander <alxndr+github@gmail.com>
2022-06-16 14:38:39 -0700

    copy: fix link to brownbase.org whoops!!

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,7 +12,7 @@ export default function About() {
 
     <h1>About Almost-Dead.net</h1>
     <p>This site is not officially affiliated with <a href="http://www.joerussosalmostdead.com/" target="_blank" rel="noopener noreferrer">Joe Russo's Almost Dead</a>.</p>
-    <p>It is inspired by fan-run sites such as Deadbase (RIP), <a href="http://phish.net" target="_blank" rel="noopener noreferrer">phish.net</a>, and <a href="http://phish.net" target="_blank" rel="noopener noreferrer">brownbase.org</a>.</p>
+    <p>It is inspired by fan-run sites such as Deadbase (RIP), <a href="http://phish.net" target="_blank" rel="noopener noreferrer">phish.net</a>, and <a href="http://brownbase.org" target="_blank" rel="noopener noreferrer">brownbase.org</a>.</p>
     <p>
       <a href="https://lot.almost-dead.net/t/setlist-corrections/17" title="discussion about setlist corrections">Corrections</a>,
       {' '}
