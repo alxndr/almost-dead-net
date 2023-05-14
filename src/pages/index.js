2ac60213adb3354ffa319e825a108844c819856b
Alexander <alxndr+github@gmail.com>
2023-05-14 00:20:36 -0700

    copy: remove Twitter

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,7 +40,6 @@ export default function Index() {
           offsite
           <ul>
             <li><a className="icon mastodon" href="https://shakedown.social/@AlmostDeadNet" title="Mastodon: @AlmostDeadNet@shakedown.social" rel="me" target="_blank"><kbd>@AlmostDeadNet@shakedown.social</kbd></a></li>
-            <li><a className="icon twitter" href="https://twitter.com/AlmostDeadNet" title="Twitter: @AlmostDeadNet" target="_blank"><kbd>@AlmostDeadNet</kbd></a></li>
             <li><a className="icon github" href="https://github.com/alxndr/almost-dead-net" title="code on GitHub" target="_blank"><kbd>alxndr/almost-dead-net</kbd></a></li>
           </ul>
         </li>
