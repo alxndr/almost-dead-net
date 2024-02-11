9d1665fa6bca8572f27804e807344ef2aefb0cf5
Alexander <alxndr+github@gmail.com>
2024-02-10 20:45:09 -0800

    wip: queries

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,20 @@ module.exports = {
   plugins: [
     'gatsby-plugin-sass',
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: 'gatsby-source-sqlite',
       options: {
-        name: `src`,
-        path: `${__dirname}/data.db`,
+        fileName: './data.db',
+        queries: [
+          { idFieldName: 'id', name: 'guests',           statement: 'SELECT * FROM guests', },
+          { idFieldName: 'id', name: 'recordings',       statement: 'SELECT * FROM recordings', },
+          { idFieldName: 'id', name: 'segues',           statement: 'SELECT * FROM segues', },
+          { idFieldName: 'id', name: 'sets',             statement: 'SELECT * FROM sets', },
+          { idFieldName: 'id', name: 'shows',            statement: 'SELECT * FROM shows', },
+          { idFieldName: 'id', name: 'songperformances', statement: 'SELECT * FROM songperformances', },
+          { idFieldName: 'id', name: 'songs',            statement: 'SELECT * FROM songs', },
+          { idFieldName: 'id', name: 'teases',           statement: 'SELECT * FROM teases', },
+          { idFieldName: 'id', name: 'venues',           statement: 'SELECT * FROM venues', },
+        ],
       },
     },
   ],
